4945ce8cd115f4a5e692cfb3143b590a6d74025c
UK <hwangwonuk94@gmail.com>
2024-11-14 01:50:29 +0900

    feat: @JsonFormat

--- src/main/java/com/example/springadvanced/controller/FormatController.java
+++ src/main/java/com/example/springadvanced/controller/FormatController.java
@@ -2,6 +2,7 @@ package com.example.springadvanced.controller;
 
 import com.example.springadvanced.dto.FormatForm;
 import com.example.springadvanced.dto.JsonFormatDto;
+import com.example.springadvanced.dto.JsonFormatDtoV2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -32,6 +33,15 @@ public class FormatController {
 
     @PostMapping("/json-format/deserialize")
     public ResponseEntity<String> jsonFormatDeserialize(@RequestBody JsonFormatDto dto) {
+        return new ResponseEntity<>(
+                "dto.getPrice() = " + dto.getPrice() +
+                        " dto.getOrderDate() = " + dto.getOrderDate(),
+                HttpStatus.OK
+        );
+    }
+
+    @PostMapping("/json-format/annotation")
+    public ResponseEntity<String> jsonFormatAnnotation(@RequestBody JsonFormatDtoV2 dto) {
         return new ResponseEntity<>(
                 "dto.getPrice() = " + dto.getPrice() +
                         " dto.getOrderDate() = " + dto.getOrderDate(),
